bde16717fba606f63aa2a545a0a731b50ff1375c
unknown <ajayr1174@gmail.com>
2021-06-13 22:25:49 +0530

    script update

--- index.js
+++ index.js
@@ -1,35 +1,31 @@
 (function () {
-
   const projects = document.querySelector(".projects");
 
   let targetelem = "";
 
   projects.addEventListener("mouseenter", mouseInhandler);
   projects.addEventListener("mousemove", mouseMovehandler);
-  
 
-  
   // handlers ---------------------------------------------------------------------
 
   function mouseInhandler(e) {
-    check(e.target) && updateStyle(e.offsetX / 100, e.offsetY / 100, e.target);
+    check(e.target) && updateStyle((e.offsetX )/ 100, e.offsetY / 100, e.target);
   }
 
   function mouseMovehandler(e) {
-    targetelem = e.target
+    targetelem = e.target;
     check(e.target) && updateStyle(e.offsetX / 100, e.offsetY / 100, e.target);
     targetelem.addEventListener("mouseleave", mouseleavehandler);
   }
 
   function mouseleavehandler(e) {
-     updateStyle(0, 0, targetelem);
+    check(e.target) && updateStyle(0, 0, targetelem);
   }
 
-//  checking and updating------------------------------------------------------------
+  //  checking and updating------------------------------------------------------------
 
-
-  function check(elem){
-    if(elem.className === "project-img"){
+  function check(elem) {
+    if (elem.className === "project-img") {
       return true;
     }
     return false;
